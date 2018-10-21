5e915be5c7e997892490c648cef83b4b383c9631
pvasconcellos <pvasconcellos@gmail.com>
2018-10-20 21:27:24 -0400

    Second commit

--- standardplot.py
+++ standardplot.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 200, 1000)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
